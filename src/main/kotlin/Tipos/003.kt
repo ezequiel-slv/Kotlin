d5d332a15737327c596696cf1129e0b1123c6bed
Ezequiel <ezequielsilvalima01@gmail.com>
2024-08-13 17:17:33 -0300

    commit

---
+++ src/main/kotlin/Tipos/003.kt
@@ -0,0 +1,14 @@
+package Tipos
+
+fun main(){
+    val product = "Smartphone"
+    val price = 4.500
+    val year = 2024
+
+    println(product::class)
+    println(price::class)
+    println(year::class)
+}
+
+// ::class busca o tipo da variável
+// String, Double e Int respectivamente
