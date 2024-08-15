435fe519fa3fa6223a40f4e640899e5d7c176fdc
Ezequiel <ezequielsilvalima01@gmail.com>
2024-08-15 15:37:10 -0300

    commit

---
+++ src/main/kotlin/alocação/006.kt
@@ -0,0 +1,14 @@
+package alocação
+fun main(){
+     // CONVERSÃO DE TIPOS E BOOLEANS
+
+    //conversão de tipos
+    val mediaDePreco = 22.4
+
+    val resposta = mediaDePreco.toInt()
+
+    println(resposta)
+
+    //boolean
+    val boolean = true // 1 byte(8bits) (true = 1 | false = 0)
+}
