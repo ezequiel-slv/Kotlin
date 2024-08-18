644e13ab5b86407da33a8ce05fba42092f20f2e6
Ezequiel <ezequielsilvalima01@gmail.com>
2024-08-18 17:25:08 -0300

    commit

---
+++ src/main/kotlin/operadores_logicos/007.kt
@@ -0,0 +1,24 @@
+package operadores_logicos
+
+fun main (){
+
+//operadores lógicos
+
+//números
+val adicao = 32 + 2 //adição
+println(adicao)
+
+val mult = 32 * 2 //multiplicação
+println(mult)
+
+val sub = 32 - 2 //subtração
+println(sub)
+
+val div = 32 / 2 //divisão
+println(div)
+
+//concatenação
+
+val frase = "Aprendendo" + " Kotlin"
+println(frase)
+}
