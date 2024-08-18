5d251310d071c54e8ee8cf0a2bd3df2608c68e76
Ezequiel <ezequielsilvalima01@gmail.com>
2024-08-18 18:08:24 -0300

    commit

---
+++ src/main/kotlin/tipos_especiais/008.kt
@@ -0,0 +1,14 @@
+package tipos_especiais
+
+fun main (){
+//números
+    val nome = "Ezequiel"
+    val sobreNome = "Silva Lima"
+    val pularLinha = '\n' // Pula uma linha
+    println(nome + pularLinha + sobreNome)
+
+    val nome2 = "Ezequiel"
+    val sobreNome2 = "Silva Lima"
+    val espaco2 = '\t' // adiciona um espaço entre os caracteres
+    println(nome + espaco2 + sobreNome)
+}
